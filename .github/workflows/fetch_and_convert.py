c0797c0d6a692f5ca91547428cc0990b313f2315
Ctory-Nily <wsad2864906247@gmail.com>
2025-02-20 22:37:58 +0800

    2025年02月20日22时37分

--- .github/workflows/fetch_and_convert.py
+++ .github/workflows/fetch_and_convert.py
@@ -182,37 +182,12 @@ if __name__ == "__main__":
             "write_yaml": True
         },
         {
-            "file_name":"DownloadCDN_CN.list", 
-            "file_urls": [
-                "https://raw.githubusercontent.com/Repcz/Tool/X/Clash/Rules/DownloadCDN_CN.list"
-                ],
-            "folder_name": "DownloadCDN_CN",
-            "write_yaml": True
-        },
-        {
             "file_name":"Emby.list", 
             "file_urls": [
                 "https://raw.githubusercontent.com/Repcz/Tool/X/Clash/Rules/Emby.list",
                 "https://raw.githubusercontent.com/kirito12827/kk_zawuku/clash/user_rule/emby.list"
                 ],
             "folder_name": "Emby",
-            "write_yaml": True
-        },
-        {
-            "file_name":"Talkatone.list", 
-            "file_urls": [
-                "https://raw.githubusercontent.com/Repcz/Tool/X/Clash/Rules/Talkatone.list"
-                ],
-            "folder_name": "Talkatone",
-            "write_yaml": True
-        },
-        {
-            "file_name":"Bilibili.list", 
-            "file_urls": [
-                "https://raw.githubusercontent.com/Repcz/Tool/X/Clash/Rules/Bilibili.list",
-                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/BiliBili/BiliBili.list",
-                ],
-            "folder_name": "Bilibili",
             "write_yaml": True
         },
         {
@@ -227,7 +202,7 @@ if __name__ == "__main__":
         {
             "file_name":"GoogleDrive.list", 
             "file_urls": [
-                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Clash/GoogleDrive/GoogleDrive.list"
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/GoogleDrive/GoogleDrive.list"
                 ],
             "folder_name": "GoogleDrive",
             "write_yaml": True
@@ -235,9 +210,217 @@ if __name__ == "__main__":
         {
             "file_name":"GoogleSearch.list", 
             "file_urls": [
-                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Clash/GoogleSearch/GoogleSearch.list"
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/GoogleSearch/GoogleSearch.list"
                 ],
             "folder_name": "GoogleSearch",
+            "write_yaml": True
+        },
+        {
+            "file_name":"Microsoft.list", 
+            "file_urls": [
+                "hhttps://raw.githubusercontent.com/Repcz/Tool/X/Clash/Rules/Microsoft.list",
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Microsoft/Microsoft.list"
+                ],
+            "folder_name": "Microsoft",
+            "write_yaml": True
+        },
+        {
+            "file_name":"YouTube.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/YouTube/YouTube.list",
+                "https://raw.githubusercontent.com/Repcz/Tool/X/Clash/Rules/YouTube.list"
+                ],
+            "folder_name": "YouTube",
+            "write_yaml": True
+        },
+        {
+            "file_name":"YouTubeMusic.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/YouTubeMusic/YouTubeMusic.list"
+                ],
+            "folder_name": "YouTubeMusic",
+            "write_yaml": True
+        },
+        {
+            "file_name":"Telegram.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Telegram/Telegram.list",
+                "https://raw.githubusercontent.com/Repcz/Tool/X/Clash/Rules/Telegram.list"
+                ],
+            "folder_name": "Telegram",
+            "write_yaml": True
+        },
+        {
+            "file_name":"Netflix.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Netflix/Netflix.list",
+                "https://raw.githubusercontent.com/Repcz/Tool/X/Clash/Rules/Netflix.list"
+                ],
+            "folder_name": "Netflix",
+            "write_yaml": True
+        },
+        {
+            "file_name":"PayPal.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/PayPal/PayPal.list",
+                "https://raw.githubusercontent.com/Repcz/Tool/X/Clash/Rules/PayPal.list"
+                ],
+            "folder_name": "PayPal",
+            "write_yaml": True
+        },
+        {
+            "file_name":"Bahamut.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Bahamut/Bahamut.list",
+                "https://raw.githubusercontent.com/Repcz/Tool/X/Clash/Rules/Bahamut.list"
+                ],
+            "folder_name": "Bahamut",
+            "write_yaml": True
+        },
+        {
+            "file_name":"Niconico.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Clash/Niconico/Niconico.list"
+                ],
+            "folder_name": "Niconico",
+            "write_yaml": True
+        },
+        {
+            "file_name":"BiliBili.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/BiliBili/BiliBili.list",
+                "https://raw.githubusercontent.com/Repcz/Tool/X/Clash/Rules/Bilibili.list"
+                ],
+            "folder_name": "BiliBili",
+            "write_yaml": True
+        },
+        {
+            "file_name":"Instagram.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Instagram/Instagram.list",
+                "https://raw.githubusercontent.com/Repcz/Tool/X/Clash/Rules/Instagram.list"
+                ],
+            "folder_name": "Instagram",
+            "write_yaml": True
+        },
+        {
+            "file_name":"Pixiv.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Clash/Pixiv/Pixiv.list"
+                ],
+            "folder_name": "Pixiv",
+            "write_yaml": True
+        },
+        {
+            "file_name":"GoogleVoice.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Clash/GoogleVoice/GoogleVoice.list"
+                ],
+            "folder_name": "GoogleVoice",
+            "write_yaml": True
+        },
+        {
+            "file_name":"Line.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Clash/Line/Line.list"
+                ],
+            "folder_name": "Line",
+            "write_yaml": True
+        },
+        {
+            "file_name":"TikTok.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/TikTok/TikTok.list",
+                "https://raw.githubusercontent.com/Repcz/Tool/X/Clash/Rules/TikTok.list"
+                ],
+            "folder_name": "TikTok",
+            "write_yaml": True
+        },
+        {
+            "file_name":"Twitter.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Twitter/Twitter.list",
+                "https://raw.githubusercontent.com/Repcz/Tool/X/Clash/Rules/Twitter.list"
+                ],
+            "folder_name": "Twitter",
+            "write_yaml": True
+        },
+        {
+            "file_name":"OpenAI.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/OpenAI/OpenAI.list",
+                "https://raw.githubusercontent.com/Repcz/Tool/X/Clash/Rules/OpenAI.list"
+                ],
+            "folder_name": "OpenAI",
+            "write_yaml": True
+        },
+        {
+            "file_name":"GitHub.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/GitHub/GitHub.list",
+                "https://raw.githubusercontent.com/Repcz/Tool/X/Clash/Rules/Github.list"
+                ],
+            "folder_name": "GitHub",
+            "write_yaml": True
+        },
+        {
+            "file_name":"Claude.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Claude/Claude.list",
+                "https://raw.githubusercontent.com/Repcz/Tool/X/Clash/Rules/Claude.list"
+                ],
+            "folder_name": "Claude",
+            "write_yaml": True
+        },
+        {
+            "file_name":"Discord.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/refs/heads/master/rule/Clash/Discord/Discord.list"
+                ],
+            "folder_name": "Discord",
+            "write_yaml": True
+        },
+        {
+            "file_name":"Epic.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Epic/Epic.list",
+                "https://raw.githubusercontent.com/Repcz/Tool/X/Clash/Rules/Epic.list"
+                ],
+            "folder_name": "Epic",
+            "write_yaml": True
+        },
+        {
+            "file_name":"Steam.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Steam/Steam.list",
+                "https://raw.githubusercontent.com/Repcz/Tool/X/Clash/Rules/Steam.list"
+                ],
+            "folder_name": "Steam",
+            "write_yaml": True
+        },
+        {
+            "file_name":"Talkatone.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/Repcz/Tool/X/Clash/Rules/Talkatone.list"
+                ],
+            "folder_name": "Talkatone",
+            "write_yaml": True
+        },
+        {
+            "file_name":"DownloadCDN_CN.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/Repcz/Tool/X/Clash/Rules/DownloadCDN_CN.list",
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/Game/GameDownloadCN/GameDownloadCN.list"
+                ],
+            "folder_name": "DownloadCDN_CN",
+            "write_yaml": True
+        },
+        {
+            "file_name":"EHGallery.list", 
+            "file_urls": [
+                "https://raw.githubusercontent.com/blackmatrix7/ios_rule_script/master/rule/Clash/EHGallery/EHGallery.list"
+                ],
+            "folder_name": "EHGallery",
             "write_yaml": True
         },
     ]
